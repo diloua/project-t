c531c80d85b9cfe3e281b37b74cd8394eb4a77b8
Oualid El Bouanani <oualid@klang-games.com>
2024-11-02 13:55:13 +0100

    task creation and structure

--- handler/handler.go
+++ handler/handler.go
@@ -1,22 +1,38 @@
 package handler
 
 import (
-    "internal/task_service"
+    "project-t/internal"
     "net/http"
     "github.com/labstack/echo/v4"
 )
+var taskService = &internal.TaskService{}
+
 func Home(c echo.Context) error {
     return c.String(http.StatusOK, "Hello, World!")
 }
 
 func GetTasks(c echo.Context) error {
-    return c.String(http.StatusOK, "Get all tasks")  
+    tasks := taskService.GetTasks()
+    return c.JSON(http.StatusOK, tasks)
 }
 
 func CreateTask(c echo.Context) error {
-    return c.String(http.StatusOK, "Create a new task")
-}
+    var task internal.Task
+    if err := c.Bind(&task); err != nil {
+        return c.JSON(http.StatusBadRequest, map[string]string{
+            "error": "Invalid task data",
+        })
+    }
 
+    createdTask, err := taskService.CreateTask(task)
+    if err != nil {
+        return c.JSON(http.StatusBadRequest, map[string]string{
+            "error": err.Error(),
+        })
+    }
+
+    return c.JSON(http.StatusCreated, createdTask)
+}
 func DeleteTask(c echo.Context) error {
     return c.String(http.StatusOK, "Delete a task")
 }
--- internal/task_service.go
+++ internal/task_service.go
@@ -1,43 +1,33 @@
 package internal
 
 import "errors"
-
-type Task struct {
-    ID          int
-    Name        string
-    Description string
-    Status      string
-}
-
 var tasks = []Task{}
 
 var nextID = 1
+type TaskService struct{}
 
-func CreateTask(name string, description string) (Task, error) {
-    if name == "" {
+func (ts *TaskService) CreateTask(task Task) (Task, error) {
+    if task.Name == "" {
         return Task{}, errors.New("Name is required")
     }
-
-    task := Task{
-        ID:          nextID,
-        Name:        name,
-        Description: description,
-        Status:      "Pending",
+    if task.Complexity == "" {
+        return Task{}, errors.New("Complexity is required")
     }
-
+    task.Id = nextID
+    task.Status = "Pending"
     nextID++
     tasks = append(tasks, task)
 
     return task, nil
 }
 
-func GetTasks() []Task {
+func (ts *TaskService) GetTasks() []Task {
     return tasks
 }
 
-func DeleteTask(id int) error {
+func (ts *TaskService) DeleteTask(id int) error {
     for i, task := range tasks {
-        if task.ID == id {
+        if task.Id == id {
             tasks = append(tasks[:i], tasks[i+1:]...)
             return nil
         }
